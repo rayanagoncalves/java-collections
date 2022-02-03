c3467cb8b2d67bd63ba646b2444ec1eceee96ecd
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2022-02-03 11:56:53 -0300

    Utilizando equals e hashcode

--- src/Aluno.java
+++ src/Aluno.java
@@ -4,6 +4,9 @@ public class Aluno {
     private int matricula;
 
     public Aluno(String nome, int matricula) {
+        if(nome == null) {
+            throw new NullPointerException();
+        }
         this.nome = nome;
         this.matricula = matricula;
     }
@@ -22,5 +25,16 @@ public class Aluno {
                 "nome='" + this.nome + '\'' +
                 ", matricula=" + this.matricula +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Aluno outro = (Aluno) obj;
+        return this.nome.equals(outro.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return this.nome.hashCode();
     }
 }
